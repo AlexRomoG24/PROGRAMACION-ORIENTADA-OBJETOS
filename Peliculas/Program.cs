5275809594eaeae1a7347063f9c6bbe96176ba25
ALEX GARCIA ROMO <60378769+AlexRomoG24@users.noreply.github.com>
2020-02-21 12:27:06 -0800

    Actividad 1

---
+++ Peliculas/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Peliculas
+{ 
+    class Peliculas
+{
+    public string Titulo;
+
+    public Int16 Año;
+
+    public string Pais;
+
+    public string Director
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
