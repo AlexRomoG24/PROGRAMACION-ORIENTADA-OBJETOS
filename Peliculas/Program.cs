aef42d8f0d67c4d7d14805f8830e5ef4cefd125b
ALEX GARCIA ROMO <60378769+AlexRomoG24@users.noreply.github.com>
2020-02-21 12:55:13 -0800

    backaup

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -16,7 +16,21 @@ namespace Peliculas
     {
         static void Main(string[] args)
         {
-            
+            Peliculas p1=new Peliculas();
+            p1.Titulo="Spiderman-2";
+            p1.Año=2004;
+            p1.Director="alex";
+            Console.WriteLine(p1.Titulo);
+            Console.WriteLine(p1.Año);
+            Console.WriteLine(p1.Director);
+
+            Peliculas p2=new Peliculas();
+            p2.Titulo="Spiderman-2";
+            p2.Año=2004;
+            p2.Director="alex";
+            Console.WriteLine(p1.Titulo);
+            Console.WriteLine(p1.Año);
+            Console.WriteLine(p1.Director);
         }
     }
 }
