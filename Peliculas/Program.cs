5afc650bb2388a1c82afe14b42b781e7a2f41ac3
ALEX GARCIA ROMO <60378769+AlexRomoG24@users.noreply.github.com>
2020-02-21 12:50:27 -0800

    prueba

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -10,13 +10,19 @@ namespace Peliculas
 
     public string Pais;
 
-    public string Director
+    public string Director;
 }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Peliculas p1=new Peliculas();
+            p1.Titulo="Spiderman-2";
+            p1.Año=2004;
+            p1.Director="alex";
+            Console.WriteLine(p1.Titulo);
+            Console.WriteLine(p1.Año);
+            Console.WriteLine(p1.Director);
         }
     }
 }
