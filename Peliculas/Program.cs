d6c43be07e1bf5c468985ecb94486e12ba0c7042
ALEX GARCIA ROMO <60378769+AlexRomoG24@users.noreply.github.com>
2020-02-21 23:35:25 -0800

    PASO 4 yupi:)

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -4,33 +4,57 @@ namespace Peliculas
 { 
     class Peliculas
 {
-    public string Titulo;
+    private string Titulo;
+    private Int16 Año;
+    private string Pais;
+    private string Director;
 
-    public Int16 Año;
-
-    public string Pais;
-
-    public string Director;
+    public string getTitulo()
+    {
+        return Titulo;
+    }
+    public void setTitulo(string T)
+    {
+        Titulo=T;
+    }
+    public Int16 getAño()
+    {
+        return Año;
+    }
+    public void setAño(Int16 T)
+    {
+        Año=T;
+    }
+    public string getPais()
+    {
+        return Pais;
+    }
+    public void setPais(string T)
+    {
+        Pais=T;
+    }
+    public string getDirector()
+    {
+        return Director;
+    }
+    public void setDirector(string T)
+    {
+        Director=T;
+    }
 }
     class Program
     {
         static void Main(string[] args)
         {
-            Peliculas p1=new Peliculas();
-            p1.Titulo="Toy Story 4";
-            p1.Año=2020;
-            p1.Director="Josh Cooley";
-            p1.Pais="EE.UU";
-            Console.WriteLine(p1.Titulo);
-            Console.WriteLine(p1.Año);
+         Peliculas p1 = new Peliculas();
+         p1.setTitulo("Toy Story 4");
+         p1.setAño(2020); 
+         Console.WriteLine("{0} ({1})", p1.getTitulo(), p1.getAño());
 
-            Peliculas p2=new Peliculas();
-            p2.Titulo="Joker";
-            p2.Año=2020;
-            p2.Director="Todd Phillips";
-            p2.Pais="EE.UU";
-            Console.WriteLine(p2.Titulo);
-            Console.WriteLine(p2.Año);
+         Peliculas p2 = new Peliculas();
+         p2.setTitulo("Joker");
+         p2.setAño(2020); 
+         Console.WriteLine("{0} ({1})", p2.getTitulo(), p2.getAño());
         }
     }
 }
