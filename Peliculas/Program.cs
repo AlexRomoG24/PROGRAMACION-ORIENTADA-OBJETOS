1dff5731f1e544d956d3474114a4641a7ab03997
ALEX GARCIA ROMO <60378769+AlexRomoG24@users.noreply.github.com>
2020-02-22 00:01:10 -0800

    Paso 5

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -8,53 +8,26 @@ namespace Peliculas
     private Int16 Año;
     private string Pais;
     private string Director;
-
-    public string getTitulo()
+    
+    public Peliculas(string Titulo,Int16 Año)
     {
-        return Titulo;
+        this.Titulo=Titulo;
+        this.Año=Año;
     }
-    public void setTitulo(string T)
+    public void imprime()
     {
-        Titulo=T;
-    }
-    public Int16 getAño()
-    {
-        return Año;
-    }
-    public void setAño(Int16 T)
-    {
-        Año=T;
-    }
-    public string getPais()
-    {
-        return Pais;
-    }
-    public void setPais(string T)
-    {
-        Pais=T;
-    }
-    public string getDirector()
-    {
-        return Director;
-    }
-    public void setDirector(string T)
-    {
-        Director=T;
+        Console.WriteLine(Titulo, Año);
     }
 }
     class Program
     {
         static void Main(string[] args)
         {
-         Peliculas p1 = new Peliculas();
-         p1.setTitulo("Toy Story 4");
-         p1.setAño(2020); 
-         Console.WriteLine("{0} ({1})", p1.getTitulo(), p1.getAño());
-
-         Peliculas p2 = new Peliculas();
-         p2.setTitulo("Joker");
-         p2.setAño(2020); 
-         Console.WriteLine("{0} ({1})", p2.getTitulo(), p2.getAño());
+            Peliculas p1 = new Peliculas("Toy Story 4",2020);
+            p1.imprime();
+            Peliculas p2 = new Peliculas("Joker",2020);
+            p2.imprime();
+            
         }
     }
 }
