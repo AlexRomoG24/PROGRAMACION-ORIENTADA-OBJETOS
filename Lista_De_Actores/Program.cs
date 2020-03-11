98f55d0a991f10d06fafcc7bc19ae3056bfd980a
ALEX GARCIA ROMO <60378769+AlexRomoG24@users.noreply.github.com>
2020-03-11 01:11:30 -0700

    Lista_de Actores

---
+++ Lista_De_Actores/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Lista_De_Actores
+{
+    public class Peliculas 
+{
+    private string Titulo;
+
+    private string Director;
+
+    private string Pais;
+
+    private short Año;
+
+    public Peliculas(string Titulo, short Año, string Director, string Pais)
+    {
+        this.Titulo = Titulo;
+        this.Año = Año;
+        this.Director = Director;
+        this.Pais = Pais;
+    }
+
+    public void Imprimir()
+    {
+        Console.WriteLine("Titulo: {0} Año: {1} Director: {2} Pais: {3}", this.Titulo, this.Año, this.Director, this.Pais);
+    }
+}
+    public class Actores
+    {
+        private string NomAct;
+        private short Nacimiento;
+
+        public Actores(string NomAct, short Nacimiento)
+        {
+            this.NomAct = NomAct;
+            this.Nacimiento = Nacimiento;
+        }
+    public void ImprimirAct() => Console.WriteLine(NomAct + ", " + Nacimiento);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Peliculas P1 = new Peliculas("Naruto la película: ¡Batalla ninja en la tierra de la nieve!", 2009, "Arturo Macías", "MEXICO");
+        P1.Imprimir();
+        List<Actores> Actors = new List<Actores>();
+        Actors.Add(new Actores("María Isabel Martiñón Fernández", 1961));
+        Actors.Add(new Actores("Víctor Ugarte Fonseca", 1976));
+        Actors.Add(new Actores("Christine Edith Byrd Prado", 1982));
+        Actors.Add(new Actores("Alfonso Obregón Inclán", 1960));
+        foreach (Actores a in Actors)
+        {
+        a.ImprimirAct();
+        }
+        }
+    }
+}
